4c486e4d135740495ad542ae2ec0df47de110cd3
Kaio Ferreira <kaioferreiracosta@hotmail.com>
2020-01-03 12:26:11 -0300

    first commit

---
+++ src/main/java/br/com/favoriterestaurant/FavoriteRestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.favoriterestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/favoriterestaurant/FavoriteRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.favoriterestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
